030bd6fa2dc21a32c3005b5556e1bbeef947b75a
Daniel Gonzalez <danny071x@gmail.com>
2021-06-22 21:01:13 -0500

    basic page functionality (no constraints)

---
+++ GonzalezDaniel-HW3/ColorChangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ColorChangeViewController.swift
+//  GonzalezDaniel-HW3
+//
+//  Created by Daniel Gonzalez on 6/22/21.
+//
+
+import UIKit
+
+class ColorChangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
